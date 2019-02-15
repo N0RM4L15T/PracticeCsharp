5e6dd529c0372e76c5c9b973e78d8af243839f82
N0RM4L15T <bypro97@naver.com>
2019-02-15 21:34:59 +0900

    Change name of method and add cocktail sort function

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -6,24 +6,22 @@ namespace Park
     {
         public delegate bool sortingDirection<T>(T a, T b) where T : IComparable;
 
-        public static bool Ascendental<T>(T a, T b) where T : IComparable
+        public static bool Ascending<T>(T a, T b) where T : IComparable
+        {
+            if (a.CompareTo(b) < 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Descending<T>(T a, T b) where T : IComparable
         {
             if (a.CompareTo(b)>0)
                 return true;
             else
                 return false;
         }
-
-        public static bool Descendental<T>(T a, T b) where T : IComparable
-        {
-            if (a.CompareTo(b)<0)
-                return true;
-            else
-                return false;
-        }
-
-       
-
+     
         public static void BubbleSort<T>(T[] target, sortingDirection<T> sort) where T : IComparable
         {
             T temp;
@@ -49,7 +47,44 @@ namespace Park
                 Console.WriteLine("Out of bounds in arrays");
             }
         }
-        
+
+        public static void CocktailSort<T>(T[] target, sortingDirection <T> sort) where T : IComparable
+        {
+            T temp;
+            int half = target.Length / 2;
+
+            try
+            {
+                for (int n1 = 0; n1 < half; n1++)
+                {
+                    int n2 = n1 + 1;
+                    while(n2 < target.Length - n1 - 1)
+                    {
+                        if (sort(target[n1], target[n2]))
+                        {
+                            temp = target[n1];
+                            target[n1] = target[n2];
+                            target[n2] = temp;
+                        }
+                        n2++;
+                    }
+                    while (n2 > n1)
+                    {
+                        if (sort(target[n2], target[n1]))
+                        {
+                            temp = target[n1];
+                            target[n1] = target[n2];
+                            target[n2] = temp;
+                        }
+                        n2--;
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Out of bounds in arrays");
+            }
+        }
     }
 
     public class MainApp
@@ -57,22 +92,27 @@ namespace Park
         static void Main(string[] args)
         {
             int[] a = new int[10] { 2, 4, 6, 8, 10, 3, 7, 5, 1, 9 };
-            PrintArray(a);
-
-
-            Sort.BubbleSort(a, new Sort.sortingDirection<int>(Sort.Ascendental));
-            PrintArray(a);
-
-            Sort.BubbleSort(a, new Sort.sortingDirection<int>(Sort.Descendental));
-            PrintArray(a);
-
             double[] b = new double[15] { 1.3, 1.5, 3.14, 2, 1, 100.54, 4.9, 8.7, 2.5, 67.36, 1579.2468, 25.4, 32.11, 11.11, 42 };
+
+            PrintArray(a);
             PrintArray(b);
 
-            Sort.BubbleSort(b, new Sort.sortingDirection<double>(Sort.Descendental));
+            Sort.CocktailSort(a, new Sort.sortingDirection<int>(Sort.Ascending));
+            PrintArray(a);
+
+            Sort.CocktailSort(a, new Sort.sortingDirection<int>(Sort.Descending));
+            PrintArray(a);
+                       
+            Sort.BubbleSort(a, new Sort.sortingDirection<int>(Sort.Ascending));
+            PrintArray(a);
+
+            Sort.BubbleSort(a, new Sort.sortingDirection<int>(Sort.Descending));
+            PrintArray(a);
+
+            Sort.BubbleSort(b, new Sort.sortingDirection<double>(Sort.Ascending));
             PrintArray(b);
 
-            Sort.BubbleSort(b, new Sort.sortingDirection<double>(Sort.Ascendental));
+            Sort.BubbleSort(b, new Sort.sortingDirection<double>(Sort.Descending));
             PrintArray(b);
         }
 
